e16c535b58bfabb3bbd7ac2ab7d86fe116dc0586
mosesolamide <olamidemoses132@gmail.com>
2025-02-10 17:43:03 +0100

    animations
    a short animation

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,31 @@
 import { IoLogoJavascript } from "react-icons/io5"
 import { RiTailwindCssFill } from "react-icons/ri"
 import { FaReact,FaHtml5  } from "react-icons/fa6"
+import { motion } from "framer-motion"
 
 export default function Hero(){
+    const variants ={
+        hidden: {opacity: 0, x: -200},
+        visible: {opacity: 1, x: 0}
+    }
+    const variantsTwo ={
+        hidden: {opacity: 0, x: 200},
+        visible: {opacity: 1, x: 0}
+    }
     return(
-        <section className='grid grid-cols-1 sm:grid-cols-2 w-full place-items-center mt-10 rounded-sm p-8 gap-4'>
-            <div className="p-6 rounded-lg bg-white mx-auto">
+        <section className='grid grid-cols-1 sm:grid-cols-2 w-full place-items-center mt-10 rounded-sm p-8 gap-4' id="home">
+            <motion.div
+                className="p-6 rounded-lg bg-white mx-auto"
+                variants={variants}
+                initial="hidden"
+                animate="visible"
+                transition={{    
+                    type: "tween",
+                    ease: "easeInOut",
+                    duration: 2,
+                    repeatType: "reverse"
+                }}
+             >
                 {/* hero */}
                 <h1 className="text-4xl md:text-5xl font-bold">
                     Hi, I'm <span className="text-blue-500">Moses</span>
@@ -21,17 +41,29 @@ export default function Hero(){
                 <a href="#contact" className="text-white bg-blue-500 rounded-sm p-2 text-sm hover:bg-blue-600 transition duration-300 cursor-pointer">
                     Contact Me
                 </a>
-            </div>
+            </motion.div>
 
-            <div className=' rounded-sm p-6 '>
+            <motion.div 
+                className=' rounded-sm p-6 '
+                variants={variantsTwo}
+                initial="hidden"
+                animate="visible"
+                transition={{    
+                    type: "tween",
+                    ease: "easeInOut",
+                    duration: 2,
+                    repeatType: "reverse"
+                }}
+                
+            >
                 <h1 className='text-center my-6 font-bold text-2xl'>Skill</h1>
-                <div className='grid grid-cols-[repeat(2,minmax(50px,80px))] gap-4 place-content-center mt-8'>
+                <div className='grid grid-cols-[repeat(2,minmax(50px,80px))] gap-4 place-items-center mt-8'>
                     <div><FaHtml5 size={50} className='text-orange-500 bounceTwo delay-200' /></div>
                     <div><IoLogoJavascript size={50} className='text-[#F7DF1E] animate-custom-bounce delay-500' /></div>
                     <div><FaReact size={50} className='text-[#61DAFB] animates' /></div>
                     <div><RiTailwindCssFill size={50} className='text-sky-400 animate-pulse' /></div>
                 </div>
-            </div>
+            </motion.div>
         </section>
     )
 }
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function NavBar(){
+    const variants ={
+        hidden: {opacity: 0, y: 75},
+        visible: {opacity: 1, y: 0}
+    }
     return(
-        <header className='fixed z-50 w-full top-0 shadow-md bg-white'>
+        <motion.header 
+            className='fixed z-50 w-full top-0 shadow-md bg-white'
+            variants={variants}
+            initial="hidden"
+            animate="visible"
+            transition={{
+                duration: 2, 
+                type: "tween",
+                repeatType:'reverse',
+                ease: "easeInOut"
+            }}
+        >
             <nav className='py-4'>
                 <ul className='flex text-[#333333] gap-3 justify-center text-sm'>
                     <li><a className='hover:text-blue-500 hover:border-b-2 hover:border-blue-600 transition hover:duration-300' href="#home">Home</a></li>
@@ -11,6 +27,6 @@ export default function NavBar(){
                     <li><a className='hover:text-blue-500 hover:border-b-2 hover:border-blue-600 transition hover:duration-300' href="#contact">Contact me</a></li>
                 </ul>
             </nav>
-        </header>
+        </motion.header>
     )
 }
